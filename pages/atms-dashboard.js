da10e14a0c45a8cdbe448e3b5b18413f8923fed1
samvel <samvel.chobanyan@gmail.com>
2025-07-18 19:36:22 +0400

    asd

--- pages/atms-dashboard.js
+++ pages/atms-dashboard.js
@@ -2,6 +2,7 @@ import { InfoCard } from "../components/ui/infoCard.js";
 import { ContainerTop } from "../components/ui/containerTop.js";
 import { LineChart } from "../components/ui/LineChart.js";
 import "../components/dynamic/chartComponent.js"
+import "../components/dynamic/selectBoxJson.js"
 import "../components/static/changeIndicator.js"
 // import { ChartComponent } from "../components/dynamic/chartComponent.js";
 
@@ -60,6 +61,15 @@ class AtmsDashboard extends HTMLElement {
                         <div class="container">
                             <container-top icon="icon-chart" title="Գործարքների քանակ" link-text="Մանրամասն" link-href="/details"> </container-top>
 <!--                            <select-box-simple value="1" options='[ {"value":"1","label":"Այսօր"}, {"value":"2","label":"Այս շաբաթ"}, {"value":"3","label":"Այս ամիս"} ]'></select-box-simple>-->
+                            <select-box-json
+                                searchable
+                                name="newselect"
+                                options = '[
+                                    { "value": "today",  "label": "Այսօր" },
+                                    { "value": "week",   "label": "Այս շաբաթ", "selected": true },
+                                    { "value": "custom", "label": "Ամսաթվի միջակայք" }
+                                ]'
+                            ></select-box-json>
                             <chart-component
                                 id="pie-chart"
                                 api-url="/dashboard/transactions-in-days"

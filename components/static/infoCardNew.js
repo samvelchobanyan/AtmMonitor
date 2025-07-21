6fc1e5b43eba88fa94512ab7b9bde937acc37b2a
samvelchobanyan <samvel.chobanyan@gmail.com>
2025-07-21 14:29:27 +0400

    Add info-card-new static component

---
+++ components/static/infoCardNew.js
@@ -0,0 +1,40 @@
+// components/static/info-card-new.js
+import { BaseElement } from '../../core/base-element.js';
+
+class InfoCardNew extends BaseElement {
+  static get properties() {
+    return ['title', 'value', 'value-currency', 'value-color', 'icon', 'button-text', 'stat', 'stat-class', 'message', 'highlight', 'border'];
+  }
+
+  render() {
+    const title = this.getAttribute('title') || '';
+    const value = this.getAttribute('value') || '';
+    const valueCurrency = this.getAttribute('value-currency');
+    const valueColor = this.getAttribute('value-color') || '';
+    const icon = this.getAttribute('icon') || '';
+    const buttonText = this.getAttribute('button-text');
+    const stat = this.getAttribute('stat');
+    const statClass = this.getAttribute('stat-class') || '';
+    const message = this.getAttribute('message');
+
+    const isHighlighted = this.hasAttribute('highlight');
+    const hasBorder = this.hasAttribute('border');
+
+    this.classList.add('info', isHighlighted && 'info_highlighted', hasBorder && 'info_border');
+
+    this.innerHTML = `
+      <div class="info__top">
+        <div class="info__title">${title}</div>
+        ${icon ? `<div class="info__icon"><i class="${icon}"></i></div>` : ''}
+      </div>
+      <div class="info__bottom">
+        <div class="info__text ${valueColor}">${value}${valueCurrency ? `<span>${valueCurrency}</span>` : ''}</div>
+        ${stat ? `<div class="info__stat stat ${statClass}">${stat}</div>` : ''}
+        ${buttonText ? `<div class="btn btn_link"><span>${buttonText}</span> <i class="icon icon-chevron-right"></i></div>` : ''}
+        ${message ? `<div class="info__message message"><i class="icon icon-message"></i><span>${message}</span></div>` : ''}
+      </div>
+    `;
+  }
+}
+
+customElements.define('info-card-new', InfoCardNew);

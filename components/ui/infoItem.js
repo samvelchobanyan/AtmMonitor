7fcc527340a57b5bb2828d15bf5a4ebbeb195e53
lusine <lusine.hovseffyanss@gmail.com>
2025-08-21 17:26:38 +0400

    info item

---
+++ components/ui/infoItem.js
@@ -0,0 +1,42 @@
+export class infoItem extends HTMLElement {
+    static get observedAttributes() {
+        return ["text", "value", "data-working"];
+    }
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+
+    render() {
+        const text = this.getAttribute("text") || "";
+        const infoValue = this.getAttribute("value");
+
+        this.classList.add("info-item");
+
+        if (infoValue !== null) {
+            this.innerHTML = `
+                <span>${text}</span>
+                <span class="info-item__value">${infoValue}</span>
+            `;
+        } else {
+            const isWorking = this.getAttribute("data-working") === "true";
+            const status = isWorking ? "Աշխատող" : "Չաշխատող";
+            const statusClass = isWorking ? "info-item__status_working" : "info-item__status_not-working";
+
+            this.innerHTML = `
+                <span>${text}</span>
+                <span class="info-item__status ${statusClass}">${status}</span>
+            `;
+        }
+    }
+}
+
+customElements.define("info-item", infoItem);

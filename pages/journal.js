d321714b161dd769fd6ef3696fd7b99f31d4a796
samvel <samvel.chobanyan@gmail.com>
2025-09-23 14:11:01 +0400

    asd

--- pages/journal.js
+++ pages/journal.js
@@ -1,99 +1,5 @@
-// old
-// import { DynamicElement } from "../core/dynamic-element.js";
-// import "../components/dynamic/chartComponent.js";
-// import "../components/dynamic/infoCard.js";
-// import "../components/ui/customTab.js";
-// import "../components/ui/customRadio.js";
-// import "../components/dynamic/doughnutChart.js";
-// import "../components/dynamic/select-box-search.js";
-// import "../components/dynamic/list-view.js";
-// import "../components/dynamic/segment.js";
-
-// class journal extends DynamicElement {
-//     constructor() {
-//         super();
-//     }
-
-//     onConnected() {
-//         // this.fetchSummary();
-//     }
-
-//     async fetchSummary(region, city, searchText, cardnumber, pageNumber) {
-//         const queryString = new URLSearchParams();
-//         if (region) {
-//             queryString.append("district", region);
-//         }
-//         if (city) {
-//             queryString.append("city", city);
-//         }
-//         if (searchText) {
-//             queryString.append("searchTerm", searchText);
-//         }
-//         if (cardnumber) {
-//             queryString.append("cardnumber", cardnumber);
-//         }
-//         if (pageNumber) {
-//             queryString.append("pageNumber", pageNumber);
-//         }
-//         try {
-//             const response = await this.fetchData(`/journal/events-journal?${queryString}`);
-//             this.setState({
-//                 selectedRegion: region,
-//                 selectedCity: city,
-//                 summary: response.data,
-//                 atmId: atmId,
-//             });
-//         } catch (err) {
-//             console.error("❌ Error fetching summary:", err);
-//             this.setState({ summary: null });
-//         }
-//     }
-
-//     template() {
-//         return /*html*/ `
-//             <div class="row">
-//                 <div class="column sm-6">
-//                     <div class="container">
-//                     <div class="tabs-container">
-//                         <div class="tabs">
-//                             <custom-tab name="atms" active >Բանկոմատ</custom-tab>
-//                             <custom-tab name="cards">Քարտ</custom-tab>
-//                         </div>
-//                     </div>
-//                     <div class="tab-content" data-tab="geo">
-//                         <segment-block></segment-block>
-//                     </div>
-//                     <div class="tab-content" data-tab="atms" style="display: none">
-//                         <list-view
-//                             searchable
-//                             search-fields="text"
-//                             items='[
-
-//                                     { "text": "ՍԱՍ Սուպերմարկետ", "value": "sas_supermarket" },
-//                                     { "text": "Առևտրի կենտրոն", "value": "shopping_mall" },
-//                                     { "text": "Բենզալցակայաններ", "value": "gas_stations" },
-//                                     { "text": "Օդանավակայան", "value": "airport" },
-//                                     { "text": "Հյուրանոցներ", "value": "hotels" },
-//                                     { "text": "Ռեստորաններ", "value": "restaurants" },
-//                                     { "text": "Դեղատներ", "value": "pharmacies" }
-//                                     ]'
-//                             item-component="badge-item"
-//                         >
-//                             <template>
-//                                 <custom-checkbox id="{{value}}" value="{{value}}">{{text}}</custom-checkbox>
-//                             </template>
-//                         </list-view>
-//                     </div>
-//                 </div>
-//                 </div>
-//             </div>
-//         `;
-//     }
-// }
-
-// customElements.define("journal-page", journal);
-
 import { DynamicElement } from "../core/dynamic-element.js";
+import '../components/ui/customTab.js';
 import "../components/dynamic/list-view.js";
 import "../components/dynamic/simpleGrid.js";  // switch to simple grid
 import encode from "../assets/js/utils/encode.js";
@@ -135,18 +41,32 @@ console.log('template journal');
 
         return /*html*/ `
             <div class="row">
-                <div class="column sm-12">
-                    <div class="container">
-                        <simple-grid
-                            data-source="/journal/events-journal"
-                            columns='["server_date", "code", "card_number", "event_description"]'
-                            clickable-columns='["code"]'
-                            mode="server"
-                            per-page="10">
-                        </simple-grid>
-                    </div>
-                </div>
+        <div class="column sm-12">
+          <div class="container">
+            <div class="tabs">
+              <custom-tab name="atm" active>Բանկոմատ</custom-tab>
+              <custom-tab name="card">Քարտ</custom-tab>
             </div>
+
+            <div class="tab-content" data-tab="atm">
+              <input type="text" placeholder="Որոնել ըստ բանկոմատի ID-ի կամ հասցեի" id='atm-search' class="list-search"/>
+            </div>
+            <div class="tab-content" data-tab="card" style="display:none">
+              <input type="text" placeholder="Որոնել ըստ Քարտի" id='card-search'  class="list-search"/>
+            </div>
+
+            <div class="table-container">
+                <simple-grid
+                    data-source="/journal/events-journal"
+                    columns='["server_date", "code", "card_number", "event_description"]'
+                    clickable-columns='["code"]'
+                    mode="server"
+                    per-page="10">
+                </simple-grid>
+            </div>
+          </div>
+        </div>
+      </div>
         `;
     }
 }

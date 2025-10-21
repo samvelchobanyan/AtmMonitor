ee8dc23196c758308819c4b7f20f5ef8d1144ad2
samvel <samvel.chobanyan@gmail.com>
2025-10-21 13:46:54 +0400

    change table to grid

--- pages/cumulative.js
+++ pages/cumulative.js
@@ -1,5 +1,6 @@
 import { DynamicElement } from "../core/dynamic-element.js";
 import "../components/dynamic/simpleTable.js";
+import '../components/dynamic/simpleGrid.js';
 import "../components/dynamic/filtrationTabs.js";
 
 class Cumulative extends DynamicElement {
@@ -13,7 +14,7 @@ class Cumulative extends DynamicElement {
             console.log("listener");
 
             let link = `/analytics/cumulative-summary?${e.detail.query}`;
-            const table = this.$("simple-table");
+            const table = this.$("simple-grid");
             table.setAttribute("data-source", link);
         });
     }
@@ -24,23 +25,18 @@ class Cumulative extends DynamicElement {
             <div class="row">
                 <div class="column sm-12">
                     <div class="table-container">  
-                        <div class="container">
-                            <simple-table
+                        <div class="container">                            
+                            <simple-grid
                                 data-source="/analytics/cumulative-summary"
-                                columns='[
-                                    "province","deposit_amount","deposit_count",
-                                    "dispense_amount","dispense_count",
-                                    "exchange_eur_amount","exchange_rub_amount","exchange_usd_amount"
-                                ]'
+                                columns='["province","deposit_amount","deposit_count","dispense_amount","dispense_count","exchange_eur_amount","exchange_rub_amount","exchange_usd_amount"]'
                                 column-labels='{"province":"Մարզ","deposit_amount":"Մուտքագրված գումար",
                                 "deposit_count":"Մուտքագրված քանակ","dispense_amount":"Կանխիկացված գումար",
                                 "dispense_count":"Կանխիկացված քանակ","exchange_eur_amount":"Փոխանակված EUR գումար",
                                 "exchange_rub_amount":"Փոխանակված RUB գումար","exchange_usd_amount":"Փոխանակված USD գումար"}'
-                                exportable
-                                searchable="false"
-                                export-filename="cumulative"
-                                export-label="Ներբեռնել CSV-ն">
-                            </simple-table>
+                                column-formatters='{"deposit_amount":"currency","dispense_amount":"currency","exchange_eur_amount":"currency","exchange_rub_amount":"currency","exchange_usd_amount":"currency"}'
+                                mode="server"
+                                per-page="10">
+                            </simple-grid>
                         </div>
                     </div>
                 </div>
